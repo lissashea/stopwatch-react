5a69423b50ab4b83f5f8bfcaaa8ee07eee13030a
“Lissa <lwarshaw674@gmail.com>
2023-04-17 14:24:02 -0400

    stopwatch handle pause stop start

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      isRunning: false
+    };
+  }
+
+  handleStart = () => {
+    this.setState({ isRunning: true });
+    this.intervalID = setInterval(() => {
+      this.setState({ counter: this.state.counter + 1 });
+    }, 1000);
+  }
+
+  handlePause = () => {
+    clearInterval(this.intervalID);
+    this.setState({ isRunning: false });
+  }
+
+  handleReset = () => {
+    clearInterval(this.intervalID);
+    this.setState({ counter: 0, isRunning: false });
+  }
+
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.counter}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.handleReset}>Reset</button>
+          <button onClick={this.handleStart}>Start</button>
+          <button onClick={this.handlePause}>Pause</button>
         </div>
       </div>
     );
